1a0aad280e586e632e27825641fb900dc98db04a
Travis CI User <travis@example.org>
2019-03-21 07:34:59 +0000

    Travis CI update 10 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: fhiplot")
-  packageStartupMessage("Version 2019.03.08 at 10:07")
+  packageStartupMessage("Version 2019.03.21 at 07:22")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
--- R/themes.R
+++ R/themes.R
@@ -4,18 +4,18 @@
 #' @param base_line_size a
 #' @param base_rect_size a
 #' @export
-theme_fhi_basic = function(base_size = 14,
-                           base_family = "Avenir",
-                           base_line_size = base_size/22,
-                           base_rect_size = base_size/22){
-
-  half_line <- base_size/2
+theme_fhi_basic <- function(base_size = 14,
+                            base_family = "Avenir",
+                            base_line_size = base_size / 22,
+                            base_rect_size = base_size / 22) {
+  half_line <- base_size / 2
 
   theme_bw(
     base_size = base_size,
     base_family = base_family,
     base_line_size = base_line_size,
-    base_rect_size = base_rect_size) %+replace%
+    base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       axis.text = element_text(colour = "black", size = rel(0.8)),
       axis.ticks = element_line(colour = "black", size = rel(0.5)),
@@ -24,11 +24,12 @@ theme_fhi_basic = function(base_size = 14,
         fill = NA,
         colour = NA,
         size = rel(1)
-        ),
+      ),
       panel.grid = element_blank(),
       panel.grid.major = element_line(size = rel(0.1)),
       panel.grid.minor = element_line(size = rel(0.05)),
-      complete = TRUE)
+      complete = TRUE
+    )
 }
 
 #' theme_fhi_basic
@@ -37,15 +38,16 @@ theme_fhi_basic = function(base_size = 14,
 #' @param base_line_size a
 #' @param base_rect_size a
 #' @export
-theme_fhi_lines = function(base_size = 14,
-                           base_family = "Avenir",
-                           base_line_size = base_size/22,
-                           base_rect_size = base_size/22){
+theme_fhi_lines <- function(base_size = 14,
+                            base_family = "Avenir",
+                            base_line_size = base_size / 22,
+                            base_rect_size = base_size / 22) {
   theme_fhi_basic(
     base_size = base_size,
     base_family = base_family,
     base_line_size = base_line_size,
-    base_rect_size = base_rect_size) %+replace%
+    base_rect_size = base_rect_size
+  ) %+replace%
     theme(
       axis.text = element_text(colour = "black", size = rel(0.8)),
       axis.ticks = element_line(colour = "black", size = rel(0.5)),
@@ -58,5 +60,6 @@ theme_fhi_lines = function(base_size = 14,
       panel.grid = element_line(colour = "black"),
       panel.grid.major = element_line(size = rel(0.1)),
       panel.grid.minor = element_line(size = rel(0.05)),
-      complete = TRUE)
+      complete = TRUE
+    )
 }
