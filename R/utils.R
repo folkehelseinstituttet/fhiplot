4d6f611094344e6e305bd635352246c9b765f730
Richard White <w@rwhite.no>
2019-06-24 12:32:45 +0300

    isoweek/isoyear

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,37 @@
 # Evaluates all arguments (see #81)
 force_all <- function(...) list(...)
+
+#' isoyear_c
+#' @param date Date
+#' @export
+isoyear_c <- function(date){
+  as.character(lubridate::isoyear(date))
+}
+
+#' isoyear_n
+#' @param date Date
+#' @export
+isoyear_n <- function(date){
+  lubridate::isoyear(date)
+}
+
+#' isoweek_c
+#' @param date Date
+#' @export
+isoweek_c <- function(date){
+  formatC(lubridate::isoweek(date), width = 2, flag = 0)
+}
+
+#' isoweek_n
+#' @param date Date
+#' @export
+isoweek_n <- function(date){
+  lubridate::isoweek(date)
+}
+
+#' isoweek_isoyear
+#' @param date Date
+#' @export
+isoweek_isoyear <- function(date){
+  c(glue::glue("{isoyear}-{isoweek}", isoyear = lubridate::isoyear(date), isoweek = isoweek_c(date)))
+}
