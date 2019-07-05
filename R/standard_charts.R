a2469a8d190ae7b8189be92dca945f8e4894001f
gulfa <gunnarroe@gmail.com>
2019-07-05 11:27:25 +0200

    Adding language to chart

--- R/standard_charts.R
+++ R/standard_charts.R
@@ -2,10 +2,11 @@
 #'
 #' @param data A data.table for a season of influenza
 #' @param title the title for the chart
+#' @param lang language of labels
 #' 
 #' @import ggplot2
 #' @export make_influenza_threshold_chart
-make_influenza_threshold_chart <- function(data, title){
+make_influenza_threshold_chart <- function(data, title, lang="NB"){
   week <-  NULL
   low <-  NULL
   medium <- NULL
@@ -14,22 +15,38 @@ make_influenza_threshold_chart <- function(data, title){
   rate <- NULL
   max_week = max(data[, week])
   data$week <- factor(data$week, levels = data$week)
+
+
+  if(lang =="EN"){
+      label_very_low = "Very low"
+      label_low = "Low"
+      label_med = "Medium"
+      label_high <- "High"
+      label_very_high <- "Very High"
+      ylab <- "% of patients with ILI"
+  } else if (lang == "NB"){
+      label_very_low = "Sv\u00E6rt lav"
+      label_low = "Lav"
+      label_med = "Middels"
+      label_high <- "H\u00F8y"
+      label_very_high <- "Sv\u00E6rt h\u00F8y"
+      ylab <- "% pasienter med ILS"
+  }
   q <- ggplot(data) +
-    
-    fhiplot::theme_fhi_lines() +
+    theme_fhi_lines() +
     geom_ribbon(aes(x=week, ymin=very_high, ymax=very_high*1.1, fill="l5", group=1), alpha=1) +
     geom_ribbon(aes(x=week, ymin=high, ymax=very_high, fill="l4", group=1), alpha=1) +
     geom_ribbon(aes(x=week, ymin=medium, ymax=high, fill="l3", group=1), alpha=1) +
     geom_ribbon(aes(x=week, ymin=low, ymax=medium, fill="l2", group=1), alpha=1) +
     geom_ribbon(aes(x=week, ymin=0, ymax=low, fill="l1", group=1), alpha=0.5)+
-    fhiplot::scale_fill_fhi("Level", labels=c("l1"="Very low", "l2"="Low", "l3"="Medium",
-                                              "l4"="High", "l5"="Very high"),
+    scale_fill_fhi("Level", labels=c("l1"=label_very_low, "l2"=label_low, "l3"=label_med,
+                                     "l4"=label_high, "l5"=label_very_high),
                             palette="map_seq_complete", direction=-1) +
     
     geom_line(aes(x=week, y=rate, group=1)) +
     geom_point(aes(x=week, y=rate, group=1)) +
     ggtitle(title) + 
-    ylab("% of patients with ILI") +
+    ylab(ylab) +
     scale_y_continuous(limits=c(0,max(data[,very_high])*1.1),
                        expand = expand_scale(mult = c(0, 0))) +
     guides(fill = guide_legend(reverse = TRUE)) + 
