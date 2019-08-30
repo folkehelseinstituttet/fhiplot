edc9fc11ad84542a440d09ad4e208b4e02723abb
Travis CI User <travis@example.org>
2019-08-30 06:43:30 +0000

    Travis CI update 45 [ci skip]

--- R/aa_environments.r
+++ R/aa_environments.r
@@ -196,4 +196,4 @@ warning_color <- c(
   "med" = vals$cols$secondary["R4"],
   "hig" = vals$cols$secondary["R2"]
 )
-names(warning_color) <- c("low","med","hig")
+names(warning_color) <- c("low", "med", "hig")
