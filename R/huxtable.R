ac89a2e502ff6e29d5e5475838d1d71885b2cde8
Richard White <w@rwhite.no>
2019-08-31 20:04:56 +0300

    huxtable

---
+++ R/huxtable.R
@@ -0,0 +1,13 @@
+#' fhi_huxtable_theme
+#' @param ht A huxtable
+#' @param position Position
+#' @export
+huxtable_theme <- function(ht, position = "center") {
+  ht <- huxtable::set_top_padding(ht, 0.2)
+  ht <- huxtable::set_bottom_padding(ht, 0.2)
+  ht <- huxtable::set_background_color(ht, huxtable::evens, huxtable::everywhere, "#F2F2F2")
+  ht <- huxtable::set_bold(ht, 1, huxtable::everywhere, TRUE)
+  ht <- huxtable::set_all_borders(ht, 1)
+  ht <- huxtable::set_position(ht, position)
+  ht
+}
