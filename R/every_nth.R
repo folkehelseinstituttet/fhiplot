1119ae895ebc9232f43f952f58a0d6513879db38
Richard White <w@rwhite.no>
2020-09-01 06:48:05 +0200

    every_nth

---
+++ R/every_nth.R
@@ -0,0 +1,10 @@
+#' every_nth
+#' Useful for plotting every nth discrete value in ggplot2
+#' @param n nth discrete value
+#' @examples
+#' \dontrun{scale_x_discrete(NULL, breaks = every_nth(n=2))}
+#' @export
+every_nth <- function(n){
+  force(n)
+  return(function(x) {x[c(TRUE, rep(FALSE, n-1))]})
+}
