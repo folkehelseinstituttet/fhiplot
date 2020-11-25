6d86ab2d2511c21227634efb97277314d1a23b45
Travis CI User <travis@example.org>
2020-11-25 04:48:48 +0000

    Travis CI update 95 [ci skip]

--- R/htmltable_css.R
+++ R/htmltable_css.R
@@ -6,7 +6,7 @@ htmltable_css_table <- "width: 100%;"
 #' @param fontsize fontsize
 #' @param border_top Border top style
 #' @export htmltable_css_header
-htmltable_css_header <- function(fontsize = 16, border_top = "2px solid grey"){
+htmltable_css_header <- function(fontsize = 16, border_top = "2px solid grey") {
   retval <- glue::glue("font-size: {fontsize}px; font-weight: bold; border-top: {border_top};")
   retval
 }
@@ -14,7 +14,7 @@ htmltable_css_header <- function(fontsize = 16, border_top = "2px solid grey"){
 #' CSS for htmltable cell
 #' @param fontsize fontsize
 #' @param border_bottom Border bottom style
-htmltable_css_cell <- function(fontsize = 14, border_bottom = "1px dashed grey"){
+htmltable_css_cell <- function(fontsize = 14, border_bottom = "1px dashed grey") {
   retval <- glue::glue("padding-left: .4em; padding-right: .2em; padding-top: .4em; padding-bottom: .4em; font-size: {fontsize}px; border-bottom: {border_bottom};")
 }
 
@@ -22,11 +22,11 @@ htmltable_css_cell <- function(fontsize = 14, border_bottom = "1px dashed grey")
 #' @param x Risk value
 #' @param fontsize fontsize
 #' @param max_risk_value Max risk value
-htmltable_css_risk_1_5 <- function(x, fontsize = 14, max_risk_value = 5){
+htmltable_css_risk_1_5 <- function(x, fontsize = 14, max_risk_value = 5) {
   stopifnot(x %in% 1:5)
   color <- fhi_pal("map_seq_complete", direction = -1)(max_risk_value)[x]
   retval <- htmltable_css_cell(fontsize = fontsize)
   retval <- glue::glue("{retval} background-color: {color};")
-  if(x>=4) retval <- paste0(retval, "color: white;")
+  if (x >= 4) retval <- paste0(retval, "color: white;")
   return(retval)
 }
--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: fhiplot")
-  packageStartupMessage("Version 2020.11.23 at 12:21")
+  packageStartupMessage("Version 2020.11.25 at 04:46")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
