827d9a5bac4044ae5ba7034ca1444b2b7bf06f54
Richard White <w@rwhite.no>
2020-12-21 08:26:47 +0100

    scaling_factor inside save_a4

--- R/save_a4.R
+++ R/save_a4.R
@@ -2,22 +2,23 @@
 #' @param p plot
 #' @param filename filename
 #' @param landscape landscape dimensions?
+#' @param scaling_factor How much larger/smaller than A4?
 #' @export
-save_a4 <- function(p, filename, landscape = T) {
+save_a4 <- function(p, filename, landscape = T, scaling_factor = 1) {
   if (landscape) {
     ggsave(
       filename,
       plot = p,
-      width = 297,
-      height = 210,
+      width = 297*scaling_factor,
+      height = 210*scaling_factor,
       units = "mm"
     )
   } else {
     ggsave(
       filename,
       plot = p,
-      width = 210,
-      height = 297,
+      width = 210*scaling_factor,
+      height = 297*scaling_factor,
       units = "mm"
     )
   }
