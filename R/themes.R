957f2488c3d3be359fb67192625cfdda15587d11
Richard White <w@rwhite.no>
2021-01-22 06:55:14 +0100

    change to theme legend size

--- R/themes.R
+++ R/themes.R
@@ -188,7 +188,7 @@ theme_fhi_shiny_kht <- function(base_size = 20,
     theme(
       title = element_text(size = base_size * 0.75),
       plot.caption = element_text(hjust = 0, face= "italic"),
-      legend.key.size = unit(1, "cm"),
+      legend.key.size = unit(1.5, "cm"),
       complete = TRUE
     )
 
