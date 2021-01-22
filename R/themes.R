34c74e63df70642d64e2bacb0f9799f1c31b2ad1
Richard White <w@rwhite.no>
2021-01-22 05:15:42 +0100

    left align graph

--- R/themes.R
+++ R/themes.R
@@ -187,6 +187,7 @@ theme_fhi_shiny_kht <- function(base_size = 20,
   ) %+replace%
     theme(
       title = element_text(size = base_size * 0.75),
+      plot.caption = element_text(hjust = 0, face= "italic"),
       legend.key.size = unit(1, "cm"),
       complete = TRUE
     )
