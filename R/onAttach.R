682da00b857d5189fe5b1cadc9285d724f81e2e4
Richard Aubrey White <hello@rwhite.no>
2021-01-28 06:25:56 +0100

    jenkins

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,11 +1,9 @@
 #' @import data.table ggplot2
 #' @importFrom magrittr %>%
 .onAttach <- function(libname, pkgname) {
-  .onAttach <- function(libname, pkgname) {
-    packageStartupMessage(paste(
-      "fhiplot",
-      utils::packageDescription("fhiplot")$Version,
-      "https://folkehelseinstituttet.github.io/fhiplot"
-    ))
-  }
+  packageStartupMessage(paste(
+    "fhiplot",
+    utils::packageDescription("fhiplot")$Version,
+    "https://folkehelseinstituttet.github.io/fhiplot"
+  ))
 }
